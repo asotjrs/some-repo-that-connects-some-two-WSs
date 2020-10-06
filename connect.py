939068a8e014a798a4d580fd2766210ac910bcb9
unknown <asotjrs@gmail.com>
2020-10-07 01:21:00 +0200

    adding the price update functionnality

--- connect.py
+++ connect.py
@@ -1,30 +1,27 @@
-import active as active
+# import Libs
+from prestapyt import *
 import odoorpc
-from prestapyt import PrestaShopWebService
-from xml.etree import ElementTree
+
+
 
 # Prepare the connection to the server
-from prestapyt import PrestaShopWebServiceDict
-
 odoo = odoorpc.ODOO('localhost', port=8069)
 
 
-# Login
+# Login credentials to the odoo API
 odoo.login('dzitodoo', 'admin', 'admin')
 
 
-
-
-# import Libs
-from prestapyt import *
-#--------------------------------------------------------------------------------------------------------------------
-
+#connecting to the prestashop WS rest API
 prestashop = PrestaShopWebServiceDict("http://localhost:8080/api", "XK314MB2NT7IJJJN6XKIWPZWS1E6IXBR")
 
+#retrieving the product we wanna do some operation on (static retrieving for demonstration purposes)
 pro=prestashop.get("products",32)
 
-product_name_in_prestashop=pro['product']['name']['language'][0]['value']
 
+
+product_name_in_prestashop=pro['product']['name']['language'][0]['value']
+product_price_in_prestashop=pro['product']['price']
 
 
 
@@ -34,7 +31,7 @@ if 'product.template' in odoo.env:
 	product_ids = odoo.env['product.template'].search([])
 	call_my_dzit_product=odoo.env['product.template'].browse(29)
 	call_my_dzit_product.name=product_name_in_prestashop
-
+	call_my_dzit_product.list_price=product_price_in_prestashop
 
 
 
